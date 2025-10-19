bf9121b523925f48d73d7184265cde567562bac7
mokalabib70-collab <mokalabib70@gmail.com>
2025-10-19 23:57:16 +0300

    Add files via upload

---
+++ pr.js
@@ -0,0 +1,19 @@
+
+function pizza()
+ {
+ document.getElementById("pizza").style.display = "block";
+ document.getElementById("pasta").style.display = "none";
+ document.getElementById("starter").style.display = "none";
+ }
+function pasta()
+ {
+ document.getElementById("pizza").style.display = "none";
+ document.getElementById("pasta").style.display = "block";
+ document.getElementById("starter").style.display = "none";
+ }
+function starter()
+ {
+ document.getElementById("pizza").style.display = "none";
+ document.getElementById("pasta").style.display = "none";
+ document.getElementById("starter").style.display = "block";
+ }
